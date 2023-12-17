57bdd55568f2338a0e78e3adf178af441c894c1f
Gopinath V <133114658+gopinathfed@users.noreply.github.com>
2023-12-17 18:55:52 +0530

    Move the snake with user input

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 const canvas = document.querySelector('#board');
 const context = canvas.getContext('2d');
 
+const upKey = document.querySelector('.up');
+const downKey = document.querySelector('.down');
+const leftKey = document.querySelector('.left');
+const rightKey = document.querySelector('.right');
+
 const canvasWidth = canvas.width; 
 const canvasHeight = canvas.height; 
 
 const snakeWidth = canvasWidth / 20;
 const snakeHeight = canvasHeight / 20;
 
-let xAxis = 0;
+let UNIT = canvasHeight / 20; // UNIT = 15 
+
+let xAxis = UNIT;
 let yAxis = 0;
+
+let y = 0;
+let x = 0;
+
+upKey.addEventListener('click',()=>{
+    x = 0 ;
+    y = -UNIT;
+})
+downKey.addEventListener('click',()=>{
+    x = 0;
+    y = UNIT;
+})
+leftKey.addEventListener('click',()=>{
+    x = -UNIT;
+    y = 0;
+})
+rightKey.addEventListener('click',()=>{
+    x = UNIT;
+    y = 0;
+})
 
 resetBoard();
 
@@ -21,13 +48,15 @@ function resetBoard(){
     context.fillStyle = "#56A3A6";
     context.fillRect(0,0,500,500);
 }
+
 function drawSnakeRepeatedly(){
     setTimeout(()=>{
         resetBoard();
         drawSnake(xAxis,yAxis);
-        xAxis+=20;
-        yAxis+=0;
+        xAxis+=x;
+        yAxis+=y;
         drawSnakeRepeatedly();
-    },300);
+    },200);
 }
+
 drawSnakeRepeatedly();
