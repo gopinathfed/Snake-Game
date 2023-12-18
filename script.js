cf77960ccb5560aef7915ea3187db414e0f29998
Gopinath V <133114658+gopinathfed@users.noreply.github.com>
2023-12-18 22:05:28 +0530

    Create snake
    Create a snake and move it with user input..

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const upKey = document.querySelector('.up');
 const downKey = document.querySelector('.down');
 const leftKey = document.querySelector('.left');
 const rightKey = document.querySelector('.right');
+const startKey = document.querySelector('.start');
 
 const canvasWidth = canvas.width; 
 const canvasHeight = canvas.height; 
@@ -17,31 +18,49 @@ let UNIT = canvasHeight / 20; // UNIT = 15
 let xAxis = UNIT;
 let yAxis = 0;
 
-let y = 0;
-let x = 0;
+let yDirection = 0;
+let xDirection = UNIT;
+
+let gameStart = false;
+
+let snake = [
+    {x:0,y:0},
+    {x:0,y:0},
+    {x:0,y:0},
+    {x:0,y:0},
+    {x:0,y:0},
+    {x:0,y:0}
+];
+
+startKey.addEventListener('click',()=>{
+    gameStart = true;
+    startGame();
+});
 
 upKey.addEventListener('click',()=>{
-    x = 0 ;
-    y = -UNIT;
+    xDirection = 0 ;
+    yDirection = -UNIT;
 })
 downKey.addEventListener('click',()=>{
-    x = 0;
-    y = UNIT;
+    xDirection = 0;
+    yDirection = UNIT;
 })
 leftKey.addEventListener('click',()=>{
-    x = -UNIT;
-    y = 0;
+    xDirection = -UNIT;
+    yDirection = 0;
 })
 rightKey.addEventListener('click',()=>{
-    x = UNIT;
-    y = 0;
+    xDirection = UNIT;
+    yDirection = 0;
 })
 
 resetBoard();
 
-function drawSnake(getXAxis,getYAxis){
-    context.fillStyle = "#DB504A";
-    context.fillRect(getXAxis,getYAxis,snakeWidth,snakeHeight);
+function drawSnake(){
+    snake.forEach((snakeBody)=>{
+        context.fillStyle = "red";
+        context.fillRect(snakeBody.x,snakeBody.y,UNIT,UNIT);
+    })
 }
 
 function resetBoard(){
@@ -49,14 +68,36 @@ function resetBoard(){
     context.fillRect(0,0,500,500);
 }
 
-function drawSnakeRepeatedly(){
-    setTimeout(()=>{
-        resetBoard();
-        drawSnake(xAxis,yAxis);
-        xAxis+=x;
-        yAxis+=y;
-        drawSnakeRepeatedly();
-    },200);
+function moveSnake(){
+    snakeHead = {
+        x:snake[snake.length-1].x+xDirection,
+        y:snake[snake.length-1].y+yDirection
+    }
+    snake.push(snakeHead);
+    snake.shift();
 }
 
-drawSnakeRepeatedly();
+function drawSnakeRepeatedly(){
+    if(gameStart){
+        setTimeout(()=>{
+            resetBoard();
+            gameOver();
+            drawSnake();
+            moveSnake();
+            drawSnakeRepeatedly();
+        },200);
+    }
+}
+function gameOver(){
+    if( snake[snake.length-1].x>=canvasWidth ||
+        snake[snake.length-1].x<0 ||
+        snake[snake.length-1].y>=canvasHeight ||
+        snake[snake.length-1].y<0 ){
+            gameStart = false;
+        }
+}
+
+function startGame(){
+    drawSnake();
+    drawSnakeRepeatedly();
+}
